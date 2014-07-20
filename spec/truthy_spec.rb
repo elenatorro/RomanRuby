35bb9127f4d37d89c2f1e5e3002ec362b3fb3299
Elena Torro <elenatorro@gmail.com>
2014-07-20 15:25:43 +0200

    Step 3: Incremental roman number tested and fixed

--- spec/truthy_spec.rb
+++ spec/truthy_spec.rb
@@ -1,24 +1,24 @@
 require "rspec"
 require_relative "../Roman.rb"
 
-describe "Parse roman" do
+describe "Step one: Convert to I" do
   before :each do
 		@conversion = Conversion.new
 	end
 
-	it "Convert V to IIIII" do
-		expect(@conversion.parse("V")).to eql("IIIII")
+	it "Convert V to 5I" do
+		expect(@conversion.parse("V")).to eql("I"*5)
 	end
 
-	it "Convert IV to IIII" do
-		expect(@conversion.parse("IV")).to eql("IIII")
+	it "Convert IV to 4I" do
+		expect(@conversion.parse("IV")).to eql("I"*4)
 	end
 
-	it "Convert IX to IIIIIIIII" do
-		expect(@conversion.parse("IX")).to eql("IIIIIIIII")
+	it "Convert IX to 9I" do
+		expect(@conversion.parse("IX")).to eql("I"*9)
 	end
 
-	it "Convert XIX to IIIIIIIII" do
+	it "Convert XIX to 19I" do
 		expect(@conversion.parse("XIX")).to eql("I"*19)
 	end
 
@@ -38,7 +38,7 @@ describe "Parse roman" do
 	end
 end
 
-describe "Add two romans" do
+describe "Step 2: Add two romans" do
 	before :each do
 		@conversion = Conversion.new
 	end
@@ -46,5 +46,30 @@ describe "Add two romans" do
 	it "V add II" do
 		expect(@conversion.add_romans("V","II")).to eq("I"*7)
 	end
-  
+
+	it "CLVI add DCCCLIV" do
+		expect(@conversion.add_romans("CLVI","DCCCLIV")).to eq("I"*1010)
+	end
+end
+
+describe "Step 3: Convert from I to roman" do
+  before :each do
+		@conversion = Conversion.new
+	end
+	
+	it "IIIII to V" do
+		expect(@conversion.parse_addition("IIIII")).to eq("V")
+	end
+	it "IIIIII to VI" do
+		expect(@conversion.parse_addition("IIIIII")).to eq("VI")
+	end
+	it "15I to XV" do
+		expect(@conversion.parse_addition("I"*15)).to eq("XV")
+	end
+	it "26 to XXVI" do
+		expect(@conversion.parse_addition("I"*26)).to eq("XXVI")
+	end
+	it "19I to XIX" do
+		expect(@conversion.parse_addition("I"*19)).to eq("XIX")
+	end
 end
